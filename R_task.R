a66bb8ea424c44634004707bd7db7ad96da431bc
Michał <michal_lewsza@hotmail.com>
2020-08-11 01:29:43 +0200

    Formatted beta

--- R_task.R
+++ R_task.R
@@ -3,94 +3,61 @@ library(dplyr)
 library(stringr)
 
 Modifier <-function(input.data,
-                    min=0,
-                    max=90,
-                    step=10,
-                    threshold=NULL,
-                    dates.column.name="Date",
-                    cut.points=c(NA,NA),
-                    sep=";",
-                    dec=","){
+                    min = 0,
+                    max = 90,
+                    step = 10,
+                    threshold = NULL,
+                    date.name = "Date",
+                    cut.points = c(NA,NA),
+                    sep = ";",
+                    dec = ","){
   
   #Sprawdzenie brakujacych  danych---------------------------------------------------------------------------
-  variables.colnames <- colnames(input.data)[-(1:1)]
-  input.data[variables.colnames] <- sapply(input.data[variables.colnames], as.numeric)
+  var.names <- colnames(input.data)[-(1:1)]
+  input.data[var.names] <- sapply(input.data[var.names], as.numeric)
   if (!identical(which(is.na(input.data)), integer(0))) 
     stop("plik zrodlowy zawiera brakujace dane")
   
   #Formatowanie --------------------------------------------------------------------------------------
-  colnames(input.data)[1] <- dates.column.name
-  input.data[[dates.column.name]] <- as.Date(input.data[[dates.column.name]])  
+  colnames(input.data)[1] <- date.name
+  input.data[[date.name]] <- as.Date(input.data[[date.name]])  
 
   #Filtrowanie----------------------------------------------------------------------------------------
   if (is.na(cut.points[1]))
-    cut.points[1] <- min(input.data[[dates.column.name]])
+    cut.points[1] <- min(input.data[[date.name]])
   if (is.na(cut.points[2]))
-    cut.points[2] <- max(input.data[[dates.column.name]])
+    cut.points[2] <- max(input.data[[date.name]])
   output.data <- input.data %>%
-    dplyr::filter(.[[dates.column.name]] >= cut.points[1] & .[[dates.column.name]] <= cut.points[2])
+    dplyr::filter(.[[date.name]] >= cut.points[1] & .[[date.name]] <= cut.points[2])
  
   #Dodawanie wariantow--------------------------------------------------------------------------------
   variants.count <- (max-min)/step + 1  
-  for (variable in variables.colnames){
+  for (variable in var.names){
     s <- 0
     for (variant in c(1:variants.count)){
-      new.column.name <- paste(substr(variable, 1, 2), str_pad(s, 2, pad="0"), substr(variable, 3, 10), sep="")
-      output.data[new.column.name] <- c(stats::filter(output.data[variable], c((s/100)), method="recursive")) #poz. na liscie wspolczynnikow = rzad opoznienia
+      new.column.name <- paste(substr(variable, 1, 2), str_pad(s, 2, pad = "0"), substr(variable, 3, 10), sep = "")
+      output.data[new.column.name] <- c(stats::filter(output.data[variable], c((s/100)), method = "recursive")) #poz. na liscie wspolczynnikow = rzad opoznienia
       s <- s + step  
     }
   }
   
   #Zamiana niskich wartosci---------------------------------------------------------------------------
-  if (is.null(threshold))
-    threshold <-  0.01 * median(unlist(output.data[-(1:1)]))
-  output.data[output.data < threshold] <- 0
+  ifelse(is.null(threshold),
+    threshold <-  0.01 * median(unlist(output.data[-(1:1)])),
+    output.data[output.data < threshold] <- 0)
   
   #Ustawienie separatora i znaku dziesietnego i Export------------------------------------------------
-  output.data <- output.data[,!(colnames(output.data) %in% variables.colnames)]
-  output.data <- format(output.data, decimal.mark=dec)
-  write.table(output.data, file="output.csv", sep=sep, row.names=FALSE)
+  output.data <- output.data[,!(colnames(output.data) %in% var.names)]
+  output.data <- format(output.data, decimal.mark = dec)
+  write.table(output.data, file = "output.csv", sep = sep, row.names = FALSE)
   return(output.data)
 }
 
 
 main.tab <- read.csv2("D:\\R_data\\R_dev_task_project\\R_dev_task\\R_dev_task.csv")
 c.points <- c("2017-02-06","2017-04-30")
-result <- Modifier(input.data=main.tab, min=0, max=50, step=25, threshold=0.8, cut.points=c.points, dates.column.name="Daty", dec=",", sep="/")
+result <- Modifier(input.data=main.tab, min = 0, max = 50, step = 25, threshold = 0.8, cut.points = c.points, date.name = "Daty", dec = ",", sep = "/")
 
 
 
 
-debug(Modifier)
-Modifier(input.data=main.tab, dates.column.name="Datyy", dec=",", sep="/")
-undebug(Modifier)
-
-
-main.tab
-main.tab[["Date"]] <- as.Date(main.tab[["Date"]])  
-min(main.tab[["Date"]])
-
-main.tab$KN_XXX_30[6] <- "   "
-
-result[7,2] + 1
-
-result[result < 20] <- 0
-
-str(result)
-
-as.numeric(result$KN00_XXX_30)
-
-as.numeric(result[-(1:1)])
-
-is.list(result$KN00_XXX_30)
-is.character(result$KN00_XXX_30)
-
-result
-
-a = sub(".", dec, result[-(1:1)], fixed=TRUE)
-as.numeric(a)
-kol1 <- c(50, 50, 50) 
-kol2 <- c(1, 1, 1)
-kol3 <- c(100, 100, 100)
-df <- data.frame(kol1, kol2, kol3)
-apply(df, 2, FUN= median)
