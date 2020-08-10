d2acb92cddb0f65d869c44364b34a7ddb66c7508
Michał <michal_lewsza@hotmail.com>
2020-08-10 22:47:54 +0200

    ADDED: -default time range

--- R_task.R
+++ R_task.R
@@ -23,6 +23,10 @@ Modifier <-function(input.data,
   input.data[[dates.column.name]] <- as.Date(input.data[[dates.column.name]])  
 
   #Filtrowanie----------------------------------------------------------------------------------------
+  if (is.na(cut.points[1]))
+    cut.points[1] <- min(input.data[[dates.column.name]])
+  if (is.na(cut.points[2]))
+    cut.points[2] <- max(input.data[[dates.column.name]])
   output.data <- input.data %>%
     dplyr::filter(.[[dates.column.name]] >= cut.points[1] & .[[dates.column.name]] <= cut.points[2])
  
@@ -37,9 +41,17 @@ Modifier <-function(input.data,
     }
   }
   
+  #Zamiana niskich wartosci---------------------------------------------------------------------------
+  if (is.na(threshold))
+    threshold = 2
+  output.data[output.data < threshold] <- 0
+  
+  
+  #Ustawienie separatora i znaku dziesietnego i Export
   output.data <- output.data[,!(colnames(output.data) %in% variables.colnames)]
-  output.data <- format(output.data, decimal.mark=dec)
+  # output.data <- format(output.data, decimal.mark=dec, scientific=FALSE)
   write.table(output.data, file="output.csv", sep=sep, row.names=FALSE)
+  as.numeric(sub(".", dec, output.data[-(1:1)], fixed=TRUE))
   return(output.data)
   
 }
@@ -49,17 +61,28 @@ Modifier <-function(input.data,
 
 main.tab <- read.csv2("D:\\R_data\\R_dev_task_project\\R_dev_task\\R_dev_task.csv")
 c.points <- c("2017-02-06","2017-04-30")
-result <- Modifier(input.data=main.tab, dates.column.name="Datyy",  cut.points=c("2017-02-06","2017-04-30"), dec=".", sep="/")
+result <- Modifier(input.data=main.tab, threshold=1, dates.column.name="Datyy", dec=",", sep="/")
 
 debug(Modifier)
-Modifier(input.data=main.tab, dates.column.name="Datyy",  cut.points=c.points, dec=",", sep="/")
-
-
+Modifier(input.data=main.tab, threshold=1 , dates.column.name="Datyy",  cut.points=c.points, dec=",", sep="/")
+undebug(Modifier)
 
 
 main.tab
-
+main.tab[["Date"]] <- as.Date(main.tab[["Date"]])  
+min(main.tab[["Date"]])
 
 main.tab$KN_XXX_30[6] <- "   "
 
+result[7,2] + 1
 
+result[result < 20] <- 0
+
+str(result)
+
+as.numeric(result$KN00_XXX_30)
+
+as.numeric(result[-(1:1)])
+
+is.list(result$KN00_XXX_30)
+is.character(result$KN00_XXX_30)
